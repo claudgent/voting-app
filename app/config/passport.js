81f31d19fb21380fb7021af90a3bd7efbe581dd6
Claudio Gentile <claudgent@gmail.com>
2017-05-18 14:12:26 -0400

    adding db insertion on passport auth

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,5 +1,6 @@
 const GitHubStrategy = require('passport-github2');
 const configAuth = require('./auth');
+const User = require('../models/User');
 
 /* =====================PASSPORT CONFIG================= */
 
@@ -18,6 +19,7 @@ module.exports = (passport) => {
     callbackURL: configAuth.githubAuth.callbackURL,
   },
 (token, refreshToken, profile, done) => {
-  process.nextTick(() => done(null, profile));
-}));
+  process.nextTick(User.findOrCreate({ githubId: profile.id }, (err, user) => done(err, user)));
+},
+));
 };
