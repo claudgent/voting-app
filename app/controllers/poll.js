d3344b96db801325177a1a2982491b7b2194979d
Claudio Gentile <claudgent@gmail.com>
2017-07-10 17:24:44 -0400

    user creating new poll should add to db

--- app/controllers/poll.js
+++ app/controllers/poll.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
+const Poll = require('../models/Poll');
 
 const addtoDB = (request) => {
-  const newPoll = mongoose.model.Poll({
+  Poll.findOrCreate({
     name: request.body.title,
     choice1: request.body.choice1,
     choice2: request.body.choice2,
