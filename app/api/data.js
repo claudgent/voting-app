9dfcd2d178c16ec0927941acba479082d34b48aa
Claudio Gentile <claudgent@gmail.com>
2017-07-11 13:49:58 -0400

    fixed im proper method call

--- app/api/data.js
+++ app/api/data.js
@@ -2,7 +2,7 @@ const Poll = require('../models/Poll');
 
 const getData = function (req, res) {
       // use the find() API and pass an empty query object to retrieve all records
-  Poll.collection('polls').find({}).toArray((err, docs) => {
+  Poll.find().toArray((err, docs) => {
     if (err) throw err;
     res.send(docs);
   });
