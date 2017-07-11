e220032296a0a2a965b6f410e9295011da1f23d5
Claudio Gentile <claudgent@gmail.com>
2017-07-11 14:19:10 -0400

    send data as json using JSON.stringify

--- app/api/data.js
+++ app/api/data.js
@@ -4,7 +4,7 @@ const getData = function (req, res) {
       // use the find() API and pass an empty query object to retrieve all records
   Poll.find({}, ((err, docs) => {
     if (err) throw err;
-    res.json(docs);
+    res.send(JSON.stringify(docs));
   }));
 };
 
