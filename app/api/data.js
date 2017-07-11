c735b80be1edf0e8c214f866d74bb7ac9cef179a
Claudio Gentile <claudgent@gmail.com>
2017-07-11 14:40:04 -0400

    send docs as obj

--- app/api/data.js
+++ app/api/data.js
@@ -3,8 +3,8 @@ const Poll = require('../models/Poll');
 const getData = function (req, res) {
       // use the find() API and pass an empty query object to retrieve all records
   Poll.find({}, ((err, docs) => {
-    if (err) throw err;
-    res.send(JSON.stringify(docs));
+    if (err) res.send('Error retrieving data');
+    res.json({ docs });
   }));
 };
 
