934ee576f57d2c6653483b2701c9a3352f5a9704
Claudio Gentile <claudgent@gmail.com>
2017-07-11 10:58:16 -0400

    remove viewModel, I do not think it is necessary

--- app/controllers/show.js
+++ app/controllers/show.js
@@ -1,21 +1,14 @@
 const Poll = require('../models/Poll');
 
 const showPoll = function (req, res) {
-  const viewModel = {
-    title: '',
-    choice1: '',
-    choice2: '',
-  };
+  
     // find the poll by searching the filename matching the url parameter
   Poll.findOne({ name: req.params.pollName },
     (err, poll) => {
       if (err) { throw err; }
       if (poll) {
-        // save the poll object to the viewModel:
-        viewModel.poll = poll;
         // save the model (since it has been updated)
-        poll.save();
-        res.render('poll', viewModel);
+        res.render('poll', poll);
       } else {
         // if no poll was found, simply go back to the homepage:
         res.send('no poll found');
