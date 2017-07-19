3d4457f00369ae6252678e39a7eab6497b105857
Claudio Gentile <claudgent@gmail.com>
2017-07-19 12:41:41 -0400

    saved chart.js file as empty to stop duplicate vars

--- public/scripts/chart.js
+++ public/scripts/chart.js
@@ -1,18 +1,1 @@
-const ctx = document.getElementById('pieChart');
-const pieChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: [choice1, choice2],
-    datasets: [{
-      label: '# of Votes',
-      data: [3, 5],
-      backgroundColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-      ],
-    }],
-  },
-  options: {
-    responsible: false,
-  },
-});
+
