c1d8734035b029add906df962c369df45e8ff13d
Claudio Gentile <claudgent@gmail.com>
2017-07-19 12:55:24 -0400

    checking that i can use handlbars props in a seperate script file

--- public/scripts/chart.js
+++ public/scripts/chart.js
@@ -1,1 +1,19 @@
 
+const ctx = document.getElementById('pieChart');
+const pieChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: [{{poll.choice1}}, {{poll.choice2}}],
+    datasets: [{
+      label: '# of Votes',
+      data: [3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+      ],
+    }],
+  },
+  options: {
+    responsible: false,
+  },
+});
